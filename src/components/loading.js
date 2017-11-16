fb7d661cbd4dedfe25c8ebb65db2cf8d3608a8d9
qin <qinshuangshuang@istarfinancial.cn>
2017-11-16 10:28:58 +0800

    loading遮罩点击之后不再点击

--- src/components/loading.js
+++ src/components/loading.js
@@ -15,25 +15,27 @@ var Loading=React.createClass({
 		var that=this;
 		var flag=this.props.flag;
         return (
-        	 <div className="spinner" style={{display:`${flag?'block':'none'}`}}>
-		            <div className="spinner-container container1">
-		                <div className="circle1"></div>
-		                <div className="circle2"></div>
-		                <div className="circle3"></div>
-		                <div className="circle4"></div>
-		            </div>
-		            <div className="spinner-container container2">
-		                <div className="circle1"></div>
-		                <div className="circle2"></div>
-		                <div className="circle3"></div>
-		                <div className="circle4"></div>
-		            </div>
-		            <div className="spinner-container container3">
-		                <div className="circle1"></div>
-		                <div className="circle2"></div>
-		                <div className="circle3"></div>
-		                <div className="circle4"></div>
-		            </div>
+        	<div className="spinnerBox" style={{display:`${flag?'block':'none'}`}}>
+	        	 <div className="spinner" style={{display:`${flag?'block':'none'}`}}>
+			            <div className="spinner-container container1">
+			                <div className="circle1"></div>
+			                <div className="circle2"></div>
+			                <div className="circle3"></div>
+			                <div className="circle4"></div>
+			            </div>
+			            <div className="spinner-container container2">
+			                <div className="circle1"></div>
+			                <div className="circle2"></div>
+			                <div className="circle3"></div>
+			                <div className="circle4"></div>
+			            </div>
+			            <div className="spinner-container container3">
+			                <div className="circle1"></div>
+			                <div className="circle2"></div>
+			                <div className="circle3"></div>
+			                <div className="circle4"></div>
+			            </div>
+			   </div>
 		   </div>
         )
 	}
