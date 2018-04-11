28b17194cb36b78ff9161f42f4e2007075360b07
1004606010@qq.com <mingxin@istarfinancial.cn>
2018-04-11 20:10:26 +0800

    update

--- dist/src/libs/aes.js
+++ dist/src/libs/aes.js
@@ -13,38 +13,6 @@ var KEY = ['Z', 'N', 'D', '2', '0', '1', '7', '1', '0', '3', '0', 'A', 'P', 'I',
 // var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
 
 
-function strEnc1(text, key) {
-    console.log(key)
-    var textBytes = aesjs.utils.utf8.toBytes(text);
-    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-    var encryptedBytes = aesCtr.encrypt(textBytes);
-    var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-
-    // var textBytes = aesjs.utils.utf8.toBytes(text);
-
-    // var aesEcb = new aesjs.ModeOfOperation.ecb(key);
-    // var encryptedBytes = aesEcb.encrypt(textBytes);
-    // var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-    return encryptedHex
-}
-
-function strDec1(text, key) {
-    var encryptedBytes = aesjs.utils.hex.toBytes(text);
-    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-    var decryptedBytes = aesCtr.decrypt(encryptedBytes);
-    var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-    // var aesEcb = new aesjs.ModeOfOperation.ecb(key);
-    // var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
-
-    // // Since electronic codebook does not store state, we can 
-    // // reuse the same instance. 
-    // //var aesEcb = new aesjs.ModeOfOperation.ecb(key); 
-    // var decryptedBytes = aesEcb.decrypt(encryptedBytes);
-
-    // // Convert our bytes back into text 
-    // var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-    return decryptedText
-}
 function strEnc(word, key) {
     var key = CryptoJS.enc.Utf8.parse(key);
     var srcs = CryptoJS.enc.Utf8.parse(word);
@@ -53,10 +21,10 @@ function strEnc(word, key) {
 }
 
 /**
- * 解密
- * @param word
- * @returns {*}
- */
+* 解密
+* @param word
+* @returns {*}
+*/
 function strDec(word, key) {
     var key = CryptoJS.enc.Utf8.parse(key);
     var decrypt = CryptoJS.AES.decrypt(word, key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 });
