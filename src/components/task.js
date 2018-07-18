2f47f2902cbc74b5cb838f2a56be735c895d5151
zhangji <1070238361@qq.com>
2018-07-18 13:50:42 +0800

    1

--- src/components/task.js
+++ src/components/task.js
@@ -26,25 +26,25 @@ var Loan = React.createClass({
 
     componentWillMount: function () {
     },
-    toListDetail: function (event) {
-        var loanId = event.currentTarget.getAttribute("data-loanId");
-        var type = event.currentTarget.getAttribute("data-type");
-        var data = { loanId: loanId };
-        if (type == "JZD") {
-            var path = {
-                pathname: '/ListDetail',
-                query: data,
-            }
-        } else if (type == "KSD") {
-            var path = {
-                pathname: '/ListDetailKSD',
-                query: data,
-            }
-        } else {
-            Toast.info("数据错误", 2)
-        }
-        hashHistory.push(path);
-    },
+    // toListDetail: function (event) {
+    //     var loanId = event.currentTarget.getAttribute("data-loanId");
+    //     var type = event.currentTarget.getAttribute("data-type");
+    //     var data = { loanId: loanId };
+    //     if (type == "JZD") {
+    //         var path = {
+    //             pathname: '/ListDetail',
+    //             query: data,
+    //         }
+    //     } else if (type == "KSD") {
+    //         var path = {
+    //             pathname: '/ListDetailKSD',
+    //             query: data,
+    //         }
+    //     } else {
+    //         Toast.info("数据错误", 2)
+    //     }
+    //     hashHistory.push(path);
+    // },
 
     logoError: function (event) {
         event.target.src = "src/img/icon/logo.png";
@@ -100,27 +100,7 @@ var Loan = React.createClass({
             // }
             // that.setState({ tagArr: that.state.tagArr })
         } else {
-            api.tag("BQ", function (res) {
-                //console.log(res)
-                if (res.code == "0000") {
-                    var tagdata = JSON.parse(strDec(res.data, key1, "", ""));
-                    //console.log(tagdata);
-                    sessionStorage.setItem("homeTag", JSON.stringify(tagdata));
-                    // for (var i in tagdata) {
-                    //     that.state.tagArr.push(
-                    //         <li key={i} data-tag={tagdata[i].tagNo} data-txt={tagdata[i].tagName} data-tagId={tagdata[i].tagId} onClick={that.toList}>
-                    //             <img src={imgPath + tagdata[i].tagPic} />
-                    //             <p>{tagdata[i].tagName}</p>
-                    //         </li>)
-                    // }
-                    // that.setState({ tagArr: that.state.tagArr })
-                } else {
-                    Toast.info(res.msg, 2);
-                }
-
-            }, function () {
-                Toast.info("连接错误", 2);
-            })
+            api.
         }
 
     },
@@ -130,9 +110,8 @@ var Loan = React.createClass({
         }
         hashHistory.push(path);
     },
-    //精准贷（理财任务）
+    //（理财任务）
     jzd: function () {
-        //console.log("精准贷")
         var key1 = globalData.key;
         var toast = globalData.toast;
         var that = this;
@@ -146,148 +125,10 @@ var Loan = React.createClass({
         that.setState({
             listJZD: arr
         })
-        // if (homeLoanJZD) {
-        //     var loanList = JSON.parse(homeLoanJZD);
-        //     var arr = [];
-        //     //console.log(loanList)
-        //     for (var i in loanList) {
-        //         var theDate = loanList[i].limitType;
-        //         var theDateTxt;
-        //         switch (theDate) {
-        //             case "Y":
-        //                 theDateTxt = "年"
-        //                 break;
-        //             case "M":
-        //                 theDateTxt = "月"
-        //                 break;
-        //             case "D":
-        //                 theDateTxt = "日"
-        //                 break;
-        //             default:
-        //                 break;
-        //         }
-        //          var theDateRate = loanList[i].rateType;
-        //         var theDateRateTxt;
-        //         switch (theDateRate) {
-        //             case "Y":
-        //                 theDateRateTxt = "年"
-        //                 break;
-        //             case "M":
-        //                 theDateRateTxt = "月"
-        //                 break;
-        //             case "D":
-        //                 theDateRateTxt = "日"
-        //                 break;
-        //             default:
-        //                 break;
-        //         }
-        //         arr.push(
-        //             // <div className="capitalList" key={i} data-loanId={loanList[i].loanId} data-type={loanList[i].type} onClick={that.toListDetail}>
-        //             //     <h3>
-        //             //         <img src={imgPath + loanList[i].logo} onError={that.logoError} />
-        //             //         <span>{loanList[i].loanName}</span>
-        //             //     </h3>
-        //             //     <div className="capitalInfo">
-        //             //         <div className="limit">
-        //             //             <h2>{loanList[i].moneyMin}~{loanList[i].moneyMax}</h2>
-        //             //             <p>额度范围(元)</p>
-        //             //         </div>
-        //             //         <ul className="special">
-        //             //             <li>{loanList[i].loanTime}</li>
-        //             //             <li>{theDateRateTxt}费率{loanList[i].rate}%</li>
-        //             //             <li>贷款期限{loanList[i].limitMin}-{loanList[i].limitMax}{theDateTxt}</li>
-        //             //         </ul>
-        //             //         <div className="apply">
-        //             //             <a href="javascript:;" >申请贷款</a>
-        //             //         </div>
-        //             //     </div>
-        //             // </div>
-                   
-
-        //         )
-        //     }
-
-        //     that.setState({
-        //         listJZD: arr
-        //     })
-        // } else {
-        //     api.loanList(1, 30, "", "JZD", function (res) {
-        //         if (res.code == "0000") {
-        //             var data = JSON.parse(strDec(res.data, key1, "", ""));
-        //             //var data=res.data;
-        //             var loanList = data.list;
-		// 			//console.log(loanList)
-        //             sessionStorage.setItem("homeLoanJZD", JSON.stringify(loanList));
-        //             var arr = [];
-        //             for (var i in loanList) {
-        //                 var theDate = loanList[i].limitType;
-        //                 var theDateTxt;
-        //                 switch (theDate) {
-        //                     case "Y":
-        //                         theDateTxt = "年"
-        //                         break;
-        //                     case "M":
-        //                         theDateTxt = "月"
-        //                         break;
-        //                     case "D":
-        //                         theDateTxt = "日"
-        //                         break;
-        //                     default:
-        //                         break;
-        //                 }
-        //                   var theDateRate = loanList[i].rateType;
-        //         var theDateRateTxt;
-        //         switch (theDateRate) {
-        //             case "Y":
-        //                 theDateRateTxt = "年"
-        //                 break;
-        //             case "M":
-        //                 theDateRateTxt = "月"
-        //                 break;
-        //             case "D":
-        //                 theDateRateTxt = "日"
-        //                 break;
-        //             default:
-        //                 break;
-        //         }
-        //                 arr.push(<div className="capitalList" key={i} data-loanId={loanList[i].loanId} data-type={loanList[i].type} onClick={that.toListDetail}>
-        //                     <h3>
-        //                         <img src={imgPath + loanList[i].logo} onError={that.logoError} />
-        //                         <span>{loanList[i].loanName}</span>
-        //                     </h3>
-        //                     <div className="capitalInfo">
-        //                         <div className="limit">
-        //                             <h2>{loanList[i].moneyMin}~{loanList[i].moneyMax}</h2>
-        //                             <p>额度范围(元)</p>
-        //                         </div>
-        //                         <ul className="special">
-        //                             <li>{loanList[i].loanTime}</li>
-        //                             <li>{theDateRateTxt}利率{loanList[i].rate}%</li>
-        //                             <li>贷款期限{loanList[i].limitMin}-{loanList[i].limitMax}{theDateTxt}</li>
-        //                         </ul>
-        //                         <div className="apply">
-        //                             <a href="javascript:;">申请贷款</a>
-        //                         </div>
-        //                     </div>
-
-        //                 </div>)
-        //             }
-
-        //             that.setState({
-        //                 listJZD: arr
-        //             })
-
-        //         } else {
-        //             Toast.info("连接错误", 2);
-        //         }
-        //     }, function () {
-        //         Toast.info("连接错误", 2);
-        //     })
-        // }
+       
     },
-    //快速贷（贷款任务）
+    //（贷款任务）
     ksd: function () {
-        //console.log("快速贷")
         var key1 = globalData.key;
         var toast = globalData.toast;
         var that = this;
@@ -297,57 +138,8 @@ var Loan = React.createClass({
             var arr = [];
            // console.log(loanList)
             for (var i in loanList) {
-                var theDate = loanList[i].limitType;
-                var theDateTxt;
-                switch (theDate) {
-                    case "Y":
-                        theDateTxt = "年"
-                        break;
-                    case "M":
-                        theDateTxt = "月"
-                        break;
-                    case "D":
-                        theDateTxt = "日"
-                        break;
-                    default:
-                        break;
-                }
-                 var theDateRate = loanList[i].rateType;
-                var theDateRateTxt;
-                switch (theDateRate) {
-                    case "Y":
-                        theDateRateTxt = "年"
-                        break;
-                    case "M":
-                        theDateRateTxt = "月"
-                        break;
-                    case "D":
-                        theDateRateTxt = "日"
-                        break;
-                    default:
-                        break;
-                }
                 arr.push(
-                    // <div className="capitalList" key={i} data-loanId={loanList[i].loanId} data-type={loanList[i].type} onClick={that.toListDetail}>
-                    //     <h3>
-                    //         <img src={imgPath + loanList[i].logo} onError={that.logoError} />
-                    //         <span>{loanList[i].loanName}</span>
-                    //     </h3>
-                    //     <div className="capitalInfo">
-                    //         <div className="limit">
-                    //             <h2>{loanList[i].moneyMin}~{loanList[i].moneyMax}</h2>
-                    //             <p>额度范围(元)</p>
-                    //         </div>
-                    //         <ul className="special">
-                    //             <li>{loanList[i].loanTime}</li>
-                    //             <li>{theDateRateTxt}费率{loanList[i].rate}%</li>
-                    //             <li>贷款期限{loanList[i].limitMin}-{loanList[i].limitMax}{theDateTxt}</li>
-                    //         </ul>
-                    //         <div className="apply">
-                    //             <a href="javascript:;" >申请贷款</a>
-                    //         </div>
-                    //     </div>
-                    // </div>
+                  
                     <ul className="tasklist">
                         <li>
                             <img src="src/img/icon/product1.png" />
@@ -381,79 +173,7 @@ var Loan = React.createClass({
                 listKSD: arr
             })
         } else {
-            api.loanList(1, 30, "", "KSD", function (res) {
-                if (res.code == "0000") {
-                    var data = JSON.parse(strDec(res.data, key1, "", ""));
-                    //var data=res.data;
-                    var loanList = data.list;
-					//console.log(loanList);
-                    sessionStorage.setItem("homeLoanKSD", JSON.stringify(loanList));
-                    var arr = [];
-                    for (var i in loanList) {
-                        var theDate = loanList[i].limitType;
-                        var theDateTxt;
-                        switch (theDate) {
-                            case "Y":
-                                theDateTxt = "年"
-                                break;
-                            case "M":
-                                theDateTxt = "月"
-                                break;
-                            case "D":
-                                theDateTxt = "日"
-                                break;
-                            default:
-                                break;
-                        }
-                        
-                         var theDateRate = loanList[i].rateType;
-                var theDateRateTxt;
-                switch (theDateRate) {
-                    case "Y":
-                        theDateRateTxt = "年"
-                        break;
-                    case "M":
-                        theDateRateTxt = "月"
-                        break;
-                    case "D":
-                        theDateRateTxt = "日"
-                        break;
-                    default:
-                        break;
-                }
-                        arr.push(<div className="capitalList" key={i} data-loanId={loanList[i].loanId} data-type={loanList[i].type} onClick={that.toListDetail}>
-                            <h3>
-                                <img src={imgPath + loanList[i].logo} onError={that.logoError} />
-                                <span>{loanList[i].loanName}</span>
-                            </h3>
-                            <div className="capitalInfo">
-                                <div className="limit">
-                                    <h2>{loanList[i].moneyMin}~{loanList[i].moneyMax}</h2>
-                                    <p>额度范围(元)</p>
-                                </div>
-                                <ul className="special">
-                                    <li>{loanList[i].loanTime}</li>
-                                    <li>{theDateRateTxt}利率{loanList[i].rate}%</li>
-                                    <li>贷款期限{loanList[i].limitMin}-{loanList[i].limitMax}{theDateTxt}</li>
-                                </ul>
-                                <div className="apply">
-                                    <a href="javascript:;">申请贷款</a>
-                                </div>
-                            </div>
-
-                        </div>)
-                    }
-
-                    that.setState({
-                        listKSD: arr
-                    })
-
-                } else {
-                    Toast.info("连接错误", 2);
-                }
-            }, function () {
-                Toast.info("连接错误", 2);
-            })
+           
         }
 
 
