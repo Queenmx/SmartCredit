9375bf279ae70d6f57a65735e04bd754f01b4863
Gao <gaoxiang@istarfinancial.cn>
2018-07-26 18:18:22 +0800

    2

--- src/components/choiceBankcard.js
+++ src/components/choiceBankcard.js
@@ -3,7 +3,7 @@ import { hashHistory } from 'react-router';
 import api from './api';
 import Header from './header';
 import { globalData } from './global.js';
-import { List, WhiteSpace, WingBlank, Radio } from 'antd-mobile';
+import { List, WhiteSpace, WingBlank, Radio, Flex } from 'antd-mobile';
 import '../css/choiceBankcard.css';
 import { Item } from '../../node_modules/antd-mobile/lib/tab-bar';
 var key1 = globalData.key;
@@ -22,14 +22,14 @@ class choiceBankcard extends Component {
             bankName: '',   //银行名称(所属银行)
             basicdata: '',  //获取的所有数据
             basicdataArr: [], //新数组
+            value: 0,
         }
-
     }
 
 
     componentDidMount() {
-
         let that = this;
+        const { value } = this.state;
         // const { value } = this.state;
         api.choiceadd(function (res) {
             console.log(res)
@@ -57,12 +57,6 @@ class choiceBankcard extends Component {
                             that.choiceadd(data.list[i].id, data.list[i].cardNumber, data.list[i].mainCard, data.list[i].bankName)
                         }}
                         >
-                            {/* <RadioItem
-                                checked={value === that.state.value} onChange={() => that.onChange(that.state.value)}
-                                defaultChecked={true}
-                                disabled={false}
-                            >
-                            </RadioItem> */}
                             <WhiteSpace size="sm" />
                             <List className="list" >
                                 <List.Item extra={data.list[i].bankName} className="listimg">   <img src={require('../img/img/web/icon_14@3x.png')} /></List.Item>
@@ -78,21 +72,30 @@ class choiceBankcard extends Component {
             }
         })
     }
+
+    onChange = (value) => {
+        console.log(value)
+        console.log('checkbox');
+        this.setState({
+            value,
+        });
+        console.log(this.state.value)
+    };
+
     //cardNumber, mainCard, bankName
-    choiceadd(id, cardNumber, mainCard, bankName) {
+    choiceadd(id, cardNumber, mainCard, bankName, value) {
         this.setState({
             id: id,
             cardNumber: cardNumber,
             mainCard: mainCard,
-            bankName: bankName
+            bankName: bankName,
+            showMask: true
         }, function () {
             console.log(this.state.id)
             console.log(this.state.cardNumber)
             console.log(this.state.mainCard)
             console.log(this.state.bankName)
         })
-
-
     }
     submissionApply = () => {
 
@@ -118,6 +121,11 @@ class choiceBankcard extends Component {
         hashHistory.push(path)
     }
     render() {
+        const { value } = this.state;
+        const data = [
+            { value: 0, label: 'doctor' },
+        ];
+
         let that = this;
         return (
             <div>
@@ -132,6 +140,164 @@ class choiceBankcard extends Component {
 }
 
 export default choiceBankcard
+
+/**以上初始代码**/
+
+// import React, { Component } from 'react'
+// import { hashHistory } from 'react-router';
+// import api from './api';
+// import Header from './header';
+// import { globalData } from './global.js';
+// import { List, WhiteSpace, WingBlank, Radio, Flex } from 'antd-mobile';
+// import '../css/choiceBankcard.css';
+// import { Item } from '../../node_modules/antd-mobile/lib/tab-bar';
+// var key1 = globalData.key;
+// const RadioItem = Radio.RadioItem;
+// class choiceBankcard extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             oldid: '',       //首选id
+//             oldcardNumber: '', //首选银行卡号
+//             oldmainCard: '',   //首选 1主卡(首选打勾的) 0副卡
+//             oldbankName: '',   //首选银行名称(所属银行)
+//             id: '',         //银行卡表主键
+//             cardNumber: '', //银行卡号
+//             mainCard: '',   //1主卡(首选打勾的) 0副卡
+//             bankName: '',   //银行名称(所属银行)
+//             basicdata: '',  //获取的所有数据
+//             data: [], //新数组
+//             value: 0,
+//         }
+//     }
+
+
+//     componentDidMount() {
+//         let that = this;
+//         const { value } = this.state;
+//         // const { value } = this.state;
+//         api.choiceadd(function (res) {
+//             console.log(res)
+//             if (res.code === "0000") {
+//                 let Decdata = strDec(res.data, key1, "", "");
+//                 let data = JSON.parse(Decdata);
+//                 console.log(data)
+
+
+//                 that.setState({
+//                     basicdata: data,
+//                     oldid: data.list[0].id,
+//                     oldcardNumber: data.list[0].cardNumber,
+//                     oldmainCard: data.list[0].mainCard,
+//                     oldbankName: data.list[0].bankName
+//                 })
+//                 console.log(that.state.oldid)
+//                 console.log(that.state.oldcardNumber)
+//                 console.log(that.state.oldmainCard)
+//                 console.log(that.state.oldbankName)
+
+         
+//                 console.log(that.state.data)
+
+//             }
+//         })
+//     }
+
+//     onChange = (value) => {
+//         console.log(value)
+//         console.log('checkbox');
+//         this.setState({
+//             value,
+//         });
+//         console.log(this.state.value)
+//     };
+
+//     //cardNumber, mainCard, bankName
+//     choiceadd(id, cardNumber, mainCard, bankName, value) {
+//         this.setState({
+//             id: id,
+//             cardNumber: cardNumber,
+//             mainCard: mainCard,
+//             bankName: bankName,
+//             showMask: true
+//         }, function () {
+//             console.log(this.state.id)
+//             console.log(this.state.cardNumber)
+//             console.log(this.state.mainCard)
+//             console.log(this.state.bankName)
+//         })
+//     }
+//     submissionApply = () => {
+
+//         var mainId = this.state.oldid;
+//         var selectId = this.state.id;
+
+//         api.update(mainId, selectId, function (res) {
+//             console.log(res)
+//             if (res.code === "0000") {
+//                 let Decdata = strDec(res.data, key1, "", "");
+//                 let data = JSON.parse(Decdata);
+//                 console.log(data)
+//             }
+//         })
+//         let path = {
+//             pathname: "/getmoney",
+//             query: {
+//                 bankName: this.state.bankName,
+//                 cardNumber: this.state.cardNumber,
+//                 id: this.state.id
+//             }
+//         };
+//         hashHistory.push(path)
+//     }
+//     render() {
+//         const { value } = this.state;
+//         const data = [
+//             { value: 0, label: 'doctor' },
+//         ];
+
+//         let that = this;
+//         return (
+//             <div>
+//                 <Header title="选择银行卡" />
+//                 {/* {that.state.basicdataArr} */}
+
+//                 <div>
+//                     {/* <List>
+//                         {this.state.basicdata.map(i => (
+//                             console.log(i),
+//                             console.log(i.key),
+//                             <RadioItem
+//                                 key={i.key}
+//                                 checked={this.state.value === i.key}
+//                                 onChange={() => this.onChange(i.key)}>
+//                                 <List className="list" >
+//                                     <List.Item extra={'1'} className="listimg">   <img src={require('../img/img/web/icon_14@3x.png')} /></List.Item>
+//                                     <div className="displaylastnumber">尾号  (213)</div>
+//                                 </List>
+//                             </RadioItem>
+//                         ))}
+//                     </List> */}
+//                     {/* <div className="content" key={i} data-id={data.list[i].id} onClick={() => {
+//                         that.choiceadd(data.list[i].id, data.list[i].cardNumber, data.list[i].mainCard, data.list[i].bankName)
+//                     }}
+//                     >
+//                         <WhiteSpace size="sm" />
+//                         <List className="list" >
+//                             <List.Item extra={data.list[i].bankName} className="listimg">   <img src={require('../img/img/web/icon_14@3x.png')} /></List.Item>
+//                             <div className="displaylastnumber">尾号  ({data.list[i].cardNumber.substr(data.list[i].cardNumber.length - 4)})</div>
+//                         </List>
+//                     </div> */}
+//                 </div>
+//                 <div className="Submission">
+//                     <div type="primary" onClick={this.submissionApply} className="Submissionbtn">提交申请</div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default choiceBankcard
 
 
 
--- src/components/getmoney.js
+++ src/components/getmoney.js
@@ -1,3 +1,225 @@
+// import React, { Component } from 'react'
+// import { hashHistory } from 'react-router';
+// import Header from './header';
+// import { Toast } from 'antd-mobile';
+// import { globalData } from './global.js';
+// import { List, WhiteSpace, InputItem, WingBlank } from 'antd-mobile';
+// import api from './api';
+// var key1 = globalData.key;
+// import "../css/getmoney.css";
+
+// class getmoney extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             procedurestext: null,
+//             number: null,
+//             display: "none",
+//             id: null,
+//             bankCardName: '',//银行卡名称
+//             cardNumber: '',//银行卡号
+//             cash: '',//提现金额
+//             serviceCharge: '',//提现手续费
+//             userName: '', //用户名
+//             showMask: false
+//         }
+//     }
+//     componentDidMount() {
+//         if (this.props.location.query.id !== undefined) {
+//             this.setState({
+//                 showMask: true
+//             })
+//         }
+//         this.setState({
+//             id: this.props.location.query.id,
+//             bankCardName: this.props.location.query.bankName,
+//             cardNumber: this.props.location.query.cardNumber,
+//         })
+//     }
+//     //判断提现金额
+//     putforward = (cash) => {
+//         var blance = localStorage.getItem("blance");
+//         var putforward = document.getElementById('putforward').value.length
+//         /*
+//         * 1. 不能有空格
+//         * 2. 开头不能为0
+//         * 3. 小于100
+//         * 4. 大于等于100
+//         * 5. 没有绑定银行卡，点击提交申请，判断DOM节点DIV显示的状态，判断是否能提交
+//         * 6. 判断输入后删除数字的长度是否等于0
+//         * 7. 判断输入的最大额度,是否大于账户余额
+//         */
+//         if (/^\s+$/gi.test(document.getElementById('putforward').value)) {
+//             Toast.info("不能输入空格", );
+//             this.setState({
+//                 serviceCharge: '',
+//                 cash: ''
+//             }, function () {
+//                 console.log(this.state.serviceCharge)
+//             })
+//         } else if (document.getElementById('putforward').value.indexOf('0') == 0) {
+//             Toast.info("开头数字不能为0", );
+//             this.setState({
+//                 serviceCharge: '',
+//                 cash: ''
+//             }, function () {
+//                 console.log(this.state.serviceCharge)
+//             })
+//         } else if (cash < 100) {
+//             this.setState({
+//                 serviceCharge: 2,
+//                 cash: cash
+//             }, function () {
+//             })
+//         } else if (cash >= 100) {
+//             this.setState({
+//                 serviceCharge: cash * 0.05,
+//                 cash: cash
+//             }, function () {
+//                 console.log(this.state.serviceCharge)
+//             })
+//         }
+//         // if (hiddenPop.style.display == "none") {
+//         //     console.log(hiddenPop.style.display == "none")
+//         //     Toast.info("请先绑定银行卡再进行操作!");
+//         // }
+//         if (putforward == '' && putforward == 0) {
+//             console.log(putforward)
+//             this.setState({
+//                 serviceCharge: '',
+//                 cash: ''
+//             }, function () {
+//                 console.log(this.state.serviceCharge)
+//             })
+//         }
+//         if (Number(cash) > Number(blance)) {
+//             Toast.info("请输入有效的金额", );
+//             this.setState({
+//                 serviceCharge: ''
+//             })
+//         }
+
+//     }
+//     //添加银行卡
+//     addCard() {
+//         hashHistory.push('/addBankcard')
+//     }
+//     submissionApply = () => {
+
+//         //获取DOM节点，判断该DOM节点是在display是不是none
+//         //如果display 不是等于none 证明在页面存在，既可调用接口
+//         var hiddenPop = document.getElementById("hiddenPop")
+//         var downloadPop = document.getElementById("downloadPop")
+//         if (hiddenPop) {
+//             if (hiddenPop.style.display !== "block") {
+//                 Toast.info("请先绑定银行卡 !", );
+//             }
+//         }
+//         if (downloadPop) {
+//             if (downloadPop.style.display !== "none") {
+//                 var bankCardName = this.state.bankCardName;
+//                 var cardNumber = this.state.cardNumber;
+//                 var cash = this.state.cash;
+//                 var serviceCharge = this.state.serviceCharge;
+//                 var userName = JSON.parse(localStorage.getItem("user")).realName
+//                 this.setState({
+//                     cash: cash
+//                 })
+//                 console.log(bankCardName)
+//                 console.log(cardNumber)
+//                 console.log(serviceCharge)
+//                 console.log(cash)
+//                 console.log(userName)
+//                 api.replacecard(bankCardName, cardNumber, cash, serviceCharge, userName, function (res) {
+//                     console.log(res)
+//                     if (res.code === "0000") {
+//                         let Decdata = strDec(res.data, key1, "", "");
+//                         let data = JSON.parse(Decdata);
+//                         console.log(data)
+//                     }
+//                 })
+//             }
+//         }
+
+//     }
+//     render() {
+//         var that = this;
+//         var blance = localStorage.getItem("blance")
+//         return (
+//             <div className="mywallet">
+//                 <Header title="申请提现" />
+//                 <div className="content">
+//                     <div className="time">
+//                         <p>{blance}</p>
+//                         <p>账户余额<span></span>(元)</p>
+//                     </div>
+//                     <WhiteSpace size="sm" />
+//                     <List className="addCard"
+//                         onClick={() => {
+//                             this.addCard()
+//                         }}
+//                     >
+//                         <List.Item extra={'添加银行卡'}>
+//                             <img src={require('../img/img/web/icon_13@2x.png')} />
+//                         </List.Item>
+//                     </List>
+//                     <WhiteSpace size="sm" />
+//                     <List className="addBank" id={`${this.state.showMask == true ? 'downloadPop' : 'hiddenPop'}`}>
+//                         <List.Item extra={this.state.bankCardName} style={{ fontSize: "18px" }}>
+//                             <img src={require('../img/img/web/icon_14@3x.png')} />
+//                         </List.Item>
+//                         <div className="displaylastnumber">尾号 ({this.state.cardNumber ? this.state.cardNumber.substr(this.state.cardNumber.length - 4) : ""})</div>
+//                     </List>
+//                     <WhiteSpace size="sm" />
+//                     <List >
+//                         <InputItem
+//                             type="text"
+//                             className="putforward"
+//                             placeholder={this.state.cash}
+//                             style={{
+//                                 fontSize: "0.3rem"
+//                             }}
+//                             onChange={this.putforward}
+//                             id='putforward'
+//                         >提现金额</InputItem>
+//                     </List>
+//                     <p></p>
+//                     <List >
+//                         <InputItem
+//                             className="procedures"
+//                             placeholder={this.state.serviceCharge}
+//                             extra="元"
+//                             // disabled="disabled"
+//                             style={{ color: "#fff" }}
+//                         >提现手续费</InputItem>
+//                     </List>
+//                     <p></p>
+//                     <WingBlank>
+//                         <div className="promptContent">
+//                             <div>提现提示</div>
+//                         </div>
+//                     </WingBlank>
+//                 </div>
+//                 <WingBlank>
+//                     <div className="promptNotes">
+//                         <div >可提交提现申请时间:工作日 09: 00 一一 18: 00</div>
+//                         <div >提现手续费费率:5%</div>
+//                         <div >提现货款额度小于100元固定收取2元手续费</div>
+//                         <div >提现货款额度大于100元按照手续费费率收取</div>
+//                         <div className="promptNotestitle">预计审核时间1-2个工作日</div>
+//                     </div>
+//                 </WingBlank>
+//                 <div className="Submission">
+//                     <div type="primary" onClick={this.submissionApply} className="buttoncolor" >提交申请</div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+// export default getmoney
+
+
+
 import React, { Component } from 'react'
 import { hashHistory } from 'react-router';
 import Header from './header';
@@ -65,15 +287,9 @@ class getmoney extends Component {
             }, function () {
                 console.log(this.state.serviceCharge)
             })
-        } else if (cash < 100) {
+        } else if (cash) {
             this.setState({
-                serviceCharge: 2,
-                cash: cash
-            }, function () {
-            })
-        } else if (cash >= 100) {
-            this.setState({
-                serviceCharge: cash * 0.05,
+                serviceCharge: (cash * 0.03).toFixed(2),
                 cash: cash
             }, function () {
                 console.log(this.state.serviceCharge)
@@ -203,9 +419,7 @@ class getmoney extends Component {
                 <WingBlank>
                     <div className="promptNotes">
                         <div >可提交提现申请时间:工作日 09: 00 一一 18: 00</div>
-                        <div >提现手续费费率:5%</div>
-                        <div >提现货款额度小于100元固定收取2元手续费</div>
-                        <div >提现货款额度大于100元按照手续费费率收取</div>
+                        <div >提现手续费费率:3%</div>
                         <div className="promptNotestitle">预计审核时间1-2个工作日</div>
                     </div>
                 </WingBlank>
